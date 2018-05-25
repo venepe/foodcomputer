cec713310903c50059e1707d74e15c229cc513ea
venepe <venepe@users.noreply.github.com>
2018-05-24 21:22:32 -0500

    Try

--- logger/index.js
+++ logger/index.js
@@ -12,9 +12,12 @@ export function logData(name, status, attribute, value, comment) {
 
 export function logFile(timestamp, name, status, attribute, value, comment) {
   console.log(timestamp, name, status, attribute, value, comment);
-  const writer = csvWriter({ sendHeaders: false });
+  const writer = csvWriter({ sendHeaders:
+    false, headers: [
+      'timestamp', 'name', 'status', 'attribute', 'value', 'comment'
+    ]});
   writer.pipe(fs.createWriteStream(config.CSV_FILENAME, { flags: 'a' }));
-  writer.write({ timestamp, name, status, attribute, value, comment });
+  writer.write(timestamp, name, status, attribute, value, comment);
   writer.end();
 }
 
